e140c611b70bc0bca23ac4eaa95efe71bddd75d1
Tristan Crawford <tristanthtcrawford@gmail.com>
2021-05-27 11:35:44 -0500

    Day 2 Part 2

--- src/2.v
+++ src/2.v
@@ -2,26 +2,11 @@ import os
 import io
 import strconv
 
-fn main() {
-	data := os.open('data/2.txt') or {
-		println("Couldn't Open Data File")
-		return
-	}
+fn process(og_ops []i64, noun i64, verb i64) i64 {
+	mut ops := og_ops.clone()
 
-	mut buffer := io.new_buffered_reader(
-		reader: data
-	)
-
-	line := buffer.read_line() or { return }
-
-	mut ops := []i64{}
-
-	for char in line.split(',') {
-		ops << strconv.parse_int(char, 10, 64)
-	}
-
-	ops[1] = 12
-	ops[2] = 2
+	ops[1] = noun
+	ops[2] = verb
 
 	mut i := 0
 	for i < ops.len {
@@ -46,5 +31,37 @@ fn main() {
 		i += 4
 	}
 
-	println('Result: ${ops[0]}')
+	return ops[0]
+}
+
+fn main() {
+	data := os.open('data/2.txt') or {
+		println("Couldn't Open Data File")
+		return
+	}
+
+	mut buffer := io.new_buffered_reader(
+		reader: data
+	)
+
+	line := buffer.read_line() or { return }
+
+	mut ops_og := []i64{}
+
+	for char in line.split(',') {
+		ops_og << strconv.parse_int(char, 10, 64)
+	}
+
+	mut result := i64(0)
+
+	outer: for n in 0..100 {
+		for v in 0..100 {
+			if process(ops_og, n, v) == 19690720 {
+				result = 100 * n + v
+				break outer
+			}
+		}
+	}
+
+	println('Result: $result')
 }
